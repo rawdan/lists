0cfe6954b4d997fca49dbf151e07d4866bcf5ea5
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-07 17:22:01 +0300

    refactor: add indentation to beautify code

--- lib/lists.ex
+++ lib/lists.ex
@@ -1,11 +1,11 @@
 defmodule Lists do
 
-  def len([]),    do: 0
-  def len([_h|t]), do: 1 + len(t)
+  def len([]),       do: 0
+  def len([_h|t]),   do: 1 + len(t)
 
 
-  def sum([]),    do: 0
-  def sum([h|t]), do: h + sum(t)
+  def sum([]),       do: 0
+  def sum([h|t]),    do: h + sum(t)
 
   def double([]),    do: []
   def double([h|t]), do: [ 2*h | double(t) ]
