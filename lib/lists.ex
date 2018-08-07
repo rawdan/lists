e96e8d58ab5f7f5e1a232bb4c62563b028e9a341
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-07 17:19:51 +0300

    add function that doubles list elements

--- lib/lists.ex
+++ lib/lists.ex
@@ -1,9 +1,13 @@
 defmodule Lists do
 
   def len([]),    do: 0
-  def len([h|t]), do: 1 + len(t)
+  def len([_h|t]), do: 1 + len(t)
 
 
   def sum([]),    do: 0
   def sum([h|t]), do: h + sum(t)
+
+  def double([]),    do: []
+  def double([h|t]), do: [ 2*h | double(t) ]
+
 end
